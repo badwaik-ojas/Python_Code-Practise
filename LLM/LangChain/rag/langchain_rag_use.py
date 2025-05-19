a59ac630efd19b27cb1292d3cd848fe107b552d9
Ojas Badwaik <badwaik.ojas@gmail.com>
2025-05-19 15:30:12 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	renamed:    LLM/LangChain/langchain_rag_preparation.py -> LLM/LangChain/rag/langchain_rag_preparation.py
    	renamed:    LLM/LangChain/langchain_rag_use.py -> LLM/LangChain/rag/langchain_rag_use.py

--- LLM/LangChain/rag/langchain_rag_use.py
+++ LLM/LangChain/rag/langchain_rag_use.py
@@ -49,24 +49,24 @@ result = qa_chain.invoke({"query": query})
 print(f"\n--- RetrievalQA Output ---")
 print(result['result'])
 
-# from langchain.chains import create_retrieval_chain
-# from langchain.chains.combine_documents import create_stuff_documents_chain
-# from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
 
-# system_prompt = (
-#     "Use the given context to answer the question. "
-#     "If you don't know the answer, say you don't know. "
-#     "Use three sentence maximum and keep the answer concise. "
-#     "Context: {context}"
-# )
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         ("system", system_prompt),
-#         ("human", "{input}"),
-#     ]
-# )
-# question_answer_chain = create_stuff_documents_chain(llm, prompt)
-# chain = create_retrieval_chain(retriever, question_answer_chain)
+system_prompt = (
+    "Use the given context to answer the question. "
+    "If you don't know the answer, say you don't know. "
+    "Use three sentence maximum and keep the answer concise. "
+    "Context: {context}"
+)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+question_answer_chain = create_stuff_documents_chain(llm, prompt)
+chain = create_retrieval_chain(retriever, question_answer_chain)
 
-# result = chain.invoke({"input": query})
-# print(result)
+result = chain.invoke({"input": query})
+print(result)
