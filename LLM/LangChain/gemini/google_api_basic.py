37953e25e50570aa7627590e8065a54bb4f81d0a
Ojas Badwaik <badwaik.ojas@gmail.com>
2025-05-23 08:31:24 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   LLM/LangChain/gemini/google_api_basic.py

--- LLM/LangChain/gemini/google_api_basic.py
+++ LLM/LangChain/gemini/google_api_basic.py
@@ -1,6 +1,9 @@
 import google.generativeai as genai
+import os
+from dotenv import load_dotenv, find_dotenv
 
-genai.configure(api_key="AIzaSyDLIAVYV3n4sDTLSujuF3_sTSyiMIKDwFE")
+load_dotenv(find_dotenv())
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 for model in genai.list_models():
     print(f"{model.name} — {'can generate content' if 'generateContent' in model.supported_generation_methods else 'no generation'}")
